4f3b19433255948d110f4dbb82edde1a6a39b88b
Vic <any@vicliu.cn>
2019-05-10 20:59:08 +0800

    add console log

--- main.go
+++ main.go
@@ -10,17 +10,29 @@ import (
 )
 
 func sysEchoHandler(w http.ResponseWriter, r *http.Request) {
-	r.Write(w)
+	var b strings.Builder
+	r.Write(&b)
+	var s = b.String()
+	w.Write([]byte(s))
+	fmt.Println("++++++++")
+	fmt.Println(s)
+	fmt.Println("++++++++")
 }
 
 func userEchoHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.RequestURI, r.Proto)
-	fmt.Fprintf(w, "Host: %s\n", r.Host)
+	var b strings.Builder
+	fmt.Fprintf(&b, "%s %s %s\n", r.Method, r.RequestURI, r.Proto)
+	fmt.Fprintf(&b, "Host: %s\n", r.Host)
 	for key, values := range r.Header {
-		fmt.Fprintf(w, "%s: %s\n", key, strings.Join(values, ", "))
+		fmt.Fprintf(&b, "%s: %s\n", key, strings.Join(values, ", "))
 	}
-	fmt.Fprintln(w)
-	io.Copy(w, r.Body)
+	fmt.Fprintln(&b)
+	io.Copy(&b, r.Body)
+	var s = b.String()
+	w.Write([]byte(s))
+	fmt.Println("++++++++")
+	fmt.Println(s)
+	fmt.Println("++++++++")
 }
 
 func main() {
@@ -29,8 +41,8 @@ func main() {
 	flag.StringVar(&addr, "addr", "127.0.0.1:8000", "listen addr")
 	flag.StringVar(&url, "url", "", "serve url")
 	flag.Parse()
-	http.HandleFunc(path.Join("/", url, "sys"), userEchoHandler)
-	http.HandleFunc(path.Join("/", url, "user"), sysEchoHandler)
 	http.HandleFunc(path.Join("/", url), sysEchoHandler)
+	http.HandleFunc(path.Join("/", url, "sys"), sysEchoHandler)
+	http.HandleFunc(path.Join("/", url, "user"), userEchoHandler)
 	http.ListenAndServe(addr, nil)
 }
